efc19b3ddbdfba7a5587776fde9885e8b052123e
Frankmona <frankoguche1@gmail.com>
2023-05-29 17:37:52 +0100

    Added a js for the dynamic date and alos added a card for post shows

---
+++ app.js
@@ -0,0 +1,4 @@
+// ********** set date ************ used this to set the date dynamically so it can change on its own yearly
+
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
